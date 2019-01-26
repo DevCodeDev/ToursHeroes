4b5fa2f851016e2d6e014a8e0a9ce841fd0f7a81
jose manuel gonzales rivera <jgmanuelrivera@outlook.com.pe>
2019-01-26 00:16:37 -0500

    Tour de heroes con angular

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Mr.Nice'},
+    {id: 12, name: 'Thor'},
+    {id: 13, name: 'Bombasto'},
+    {id: 14, name: 'Cerelitas'},
+    {id: 15, name: 'Magneto'},
+    {id: 16, name: 'HombreHormiga'},
+    {id: 17, name: 'SpiderMan'},
+    {id: 18, name: 'IronMan'},
+    {id: 19, name: 'Hulk'},
+    {id: 20, name: 'Mr.Strange'}
+];
